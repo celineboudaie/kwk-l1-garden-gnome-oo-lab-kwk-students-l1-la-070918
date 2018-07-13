fef1b60f45d40cbe1f4cc02f5b1a341a5b3f2fb0
celineboudaie <cboudaie@gmail.com>
2018-07-13 16:37:32 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,2 +1,6 @@
 class GardenGnome
-  
+attr_accessor :name, :age, :gluten_allergy, :hat_color
+ 
+ attr_reader :personality, :gnaw, :shout, :introduce_self
+ def initialize(personality = "evil", hat_color = "red")
+   
