7b9a23403c29fed37845b0b0840fafd6032a2a4e
celineboudaie <cboudaie@gmail.com>
2018-07-13 16:39:29 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -7,4 +7,13 @@ attr_accessor :name, :age, :gluten_allergy, :hat_color
    @hat_color= hat_color
  end
  
- def 
+ def gnaw
+   return "Gnawing on a tree!!!"
+ end
+ def shout
+   return "GNARLY!!!"
+ end
+ def introduce_self
+   return "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
+ end
+end
